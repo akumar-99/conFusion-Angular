a67dd1b0b82e32fb347f240094b3b1163189b110
Ashish Kumar <ashish.dav99@gmail.com>
2020-03-27 03:57:59 +0530

    components part one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // direectly code can be written instead of path using template instead of templateUrl: ` `
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
