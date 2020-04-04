c115c9ea5311dd663612b14b633adabe0d7b39a1
Ashish Kumar <ashish.dav99@gmail.com>
2020-04-05 01:28:00 +0530

    rxjs part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -12,6 +13,35 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
   @ViewChild('fform') feedbackFormDirective;
+
+  formErrors = {
+    'firstname': "",
+    'lastname': "",
+    'telnum': "",
+    'email': ""
+  }
+
+  validationMessages = {
+    'firstname': {
+      'required': "First name is requied",
+      'minlength': "Atleast 2 characters long",
+      'maxlength': "not more than 25 characters"
+    },
+    'lastname': {
+      'required': "Last name is requied",
+      'minlength': "Atleast 2 characters long",
+      'maxlength': "not more than 25 characters"
+    },
+    'telnum': {
+      'required': "Telephone No. requied",
+      'pattern': "Should contain only numbers"
+    },
+    'email': {
+      'required': "Telephone No. is requied",
+      'email': "Invaid email format"
+    }
+  }
+
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
@@ -21,15 +51,40 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0, [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: '',
       message: ''
     });
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); //re(set) form validation messages
   }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        //clear previos error message
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for(const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
 
   onSubmit() {
     this.feedback = this.feedbackForm.value;
