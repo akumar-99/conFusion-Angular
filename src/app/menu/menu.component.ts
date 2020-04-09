5dc95513b28c56a36175f49265281eca22bde4a4
Ashish Kumar <ashish.dav99@gmail.com>
2020-04-09 09:01:57 +0530

    animations part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { Dish } from '../shared/dish'
-import { DishService } from '../services/dish.service'
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 
 export class MenuComponent implements OnInit {
@@ -26,5 +35,5 @@ export class MenuComponent implements OnInit {
   // onSelect(dish: Dish) {
   //   this.selectedDish = dish;
   // }
-BrowserAnimationsModule
+  BrowserAnimationsModule
 }
